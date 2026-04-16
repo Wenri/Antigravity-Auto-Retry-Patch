8d9178f11e32526b2b6b130b795b94cdfbcfbaa7
pannenkoekissus <143376941+pannenkoekissus@users.noreply.github.com>
2026-04-16 11:36:12 +0200

    the other thing probably works but this is nicer

--- applyAutoRetryContinueAllowPatch.js
+++ applyAutoRetryContinueAllowPatch.js
@@ -318,7 +318,7 @@ async function applyPatch() {
             return;
         }
     } catch (e) {
-        if (!isElevated) log('you do not have sufficient permissions');
+        if (!isElevated()) log('you do not have sufficient permissions');
         else if (e.code === 'EACCES') {
             warn('Permission denied while writing file.');
         } else {
